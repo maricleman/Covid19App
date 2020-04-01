87114b9d2cf09ffffe633fa70e55b06c1583e1b0
David Maricle <davidnmaricle@gmail.com>
2020-04-01 16:25:38 -0500

    Adding the initial ETL process to load data for COVID-19. This is by state.

---
+++ LoadCovid19Data/Scripts/GetLatestCovid19Data.ps1
@@ -0,0 +1,23 @@
+﻿# A quick script to get the latest version of the Covid-19 data from GitHub
+
+# .Net methods for hiding/showing the console in the background
+Add-Type -Name Window -Namespace Console -MemberDefinition '
+[DllImport("Kernel32.dll")]
+public static extern IntPtr GetConsoleWindow();
+
+[DllImport("user32.dll")]
+public static extern bool ShowWindow(IntPtr hWnd, Int32 nCmdShow);
+'
+function Hide-Console
+{
+    $consolePtr = [Console.Window]::GetConsoleWindow()
+    #0 hide
+    [Console.Window]::ShowWindow($consolePtr, 0)
+}
+Hide-Console
+
+$locationOfLocalRepository = "C:\Users\david\source\repos\covid-19-data"
+
+Set-Location $locationOfLocalRepository
+
+git pull
